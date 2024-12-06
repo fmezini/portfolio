6a0ce695cf76aced27d67ea2ff7b9949ad784ed5
Fernando Mezini <mezini@gmail.com>
2024-12-06 00:07:26 -0300

    Adding reactive forms - validation is not working yet and no changes in the tests

--- angular-test/src/app/app-routing.module.ts
+++ angular-test/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
+import { AddProductReactiveComponent } from './components/add-product-reactive/add-product-reactive.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ProductsComponent },
-  { path: 'add-product', component: AddProductComponent}
+  { path: 'add-product', component: AddProductComponent},
+  { path: 'add-product-reactive', component: AddProductReactiveComponent }
 ];
 
 @NgModule({
--- angular-test/src/app/components/add-product-reactive/add-product-reactive.component.ts
+++ angular-test/src/app/components/add-product-reactive/add-product-reactive.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product-reactive',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-product-reactive.component.html',
   styleUrl: './add-product-reactive.component.css'
 })
 export class AddProductReactiveComponent {
+
+  // using form group and form controls
+  controls = new FormGroup({
+    id: new FormControl('', [Validators.required]),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    description: new FormControl('')
+  });
+
+  addProduct() {
+    alert(this.controls.value['id'] + ' ' + this.controls.value['name'] + ' ' + this.controls.value['description']);
+  }
+
 
 }
